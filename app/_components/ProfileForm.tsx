28f7d5be70b022256652bb8a033985d2ff5efe66
Tomas Jansson <tomas.jansson@gmail.com>
2023-12-19 09:19:32 +0100

    How to make this compile?

--- app/_components/ProfileForm.tsx
+++ app/_components/ProfileForm.tsx
@@ -1,28 +1,51 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
+import {
+  FieldPath,
+  FieldValues,
+  UseControllerProps,
+  useForm,
+} from "react-hook-form";
 import * as z from "zod";
-import { ProfileFormValues, countries, formSchema } from "./profile-schema";
+import { ProfileFormValues, formSchema } from "./profile-schema";
 import { Form } from "@/components/ui/form";
 import { Button } from "@/components/ui/button";
-import { CommandItem } from "@/components/ui/command";
-import { cn } from "@/lib/utils";
-import { Check } from "lucide-react";
 import { useToast } from "@/components/ui/use-toast";
-import { useEffect } from "react";
 import { saveProfile } from "./profile-actions";
-import { ComboboxField, InputField } from "./forms";
+import { InputField } from "./forms";
+
+const SocialFields = <
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>
+>(
+  props: UseControllerProps<TFieldValues, TName>
+) => {
+  console.log("==> props", props);
+  const { control, name } = props;
+  return (
+    <>
+      {" "}
+      <InputField
+        control={control}
+        name={`${name}.twitter`}
+        label="twitter"
+        placeholder="Twitter handle"
+        description=""
+      />
+    </>
+  );
+};
 
 export const ProfileForm = () => {
   const { toast } = useToast();
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      someprop: "",
-      country: "",
-      zipCode: "",
-      city: "",
+      someprop: "some value",
+      social: {
+        twitter: "MyHandle",
+      },
     },
   });
 
@@ -32,105 +55,12 @@ export const ProfileForm = () => {
       title: "Profile updated on server",
       description: JSON.stringify({ result, values }, null, 2),
     });
-    console.log("onSubmit", result);
+    form.reset();
   }
 
-  const selectedCountry = form.watch("country");
-  const zipCodes = countries[selectedCountry]?.zipCodes ?? [];
-  const zipCode = form.watch("zipCode");
-  const zipCodeToString = ({ code, name }: { code: string; name: string }) =>
-    `${code} - ${name}`;
-
-  useEffect(() => {
-    console.log("selectedCountry", selectedCountry);
-    console.log("zipCode", zipCode);
-    if (!selectedCountry || !zipCode) form.setValue("city", "");
-    else {
-      form.setValue(
-        "city",
-        countries[selectedCountry]?.zipCodes.find((zc) => zc.code === zipCode)
-          ?.name ?? ""
-      );
-    }
-  }, [zipCode, selectedCountry]);
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <ComboboxField
-          control={form.control}
-          name="zipCode"
-          label="Zip code"
-          placeholder="Select zip code"
-          emptyText="No zip found."
-          description="This is the zip code that will be used in the dashboard."
-          itemsComponent={(field) =>
-            zipCodes.map((zc) => (
-              <CommandItem
-                value={zc.code}
-                key={zc.code}
-                onSelect={() => {
-                  form.setValue("zipCode", zc.code);
-                }}
-              >
-                <Check
-                  className={cn(
-                    "mr-2 h-4 w-4",
-                    zc.code === field.value ? "opacity-100" : "opacity-0"
-                  )}
-                />
-                {zipCodeToString(zc)}
-              </CommandItem>
-            ))
-          }
-          renderFn={(field) =>
-            field.value && zipCodes.find((zc) => zc.code === field.value)
-              ? zipCodeToString(zipCodes.find((zc) => zc.code === field.value)!)
-              : "Select zip"
-          }
-        />
-        <ComboboxField
-          control={form.control}
-          name="country"
-          label="Country"
-          placeholder="Select country"
-          emptyText="No country found."
-          description="This is the country that will be used in the dashboard."
-          itemsComponent={(field) =>
-            Object.keys(countries).map((cc) => (
-              <CommandItem
-                value={countries[cc].name}
-                key={countries[cc].code}
-                onSelect={() => {
-                  form.setValue("country", countries[cc].code);
-                }}
-              >
-                <Check
-                  className={cn(
-                    "mr-2 h-4 w-4",
-                    countries[cc].code === field.value
-                      ? "opacity-100"
-                      : "opacity-0"
-                  )}
-                />
-                {`${countries[cc].name} - ${countries[cc].code}`}
-              </CommandItem>
-            ))
-          }
-          renderFn={(field) =>
-            field.value && countries[field.value]
-              ? countries[field.value]?.name
-              : "Select country"
-          }
-        />
-        <InputField
-          control={form.control}
-          name="city"
-          label="City"
-          placeholder="Select zip code"
-          description="This is the city that will be used in the dashboard."
-          disabled
-        />
-
         <InputField
           control={form.control}
           name="someprop"
@@ -138,13 +68,7 @@ export const ProfileForm = () => {
           placeholder="tomas"
           description="This is the name that will be used in the dashboard."
         />
-        <InputField
-          control={form.control}
-          name="nestedProp.someprop2"
-          label="Name"
-          placeholder="tomas"
-          description="This is the name that will be used in the dashboard."
-        />
+        <SocialFields control={form.control} name="social" />
         <Button type="submit">Submit</Button>
       </form>
     </Form>
